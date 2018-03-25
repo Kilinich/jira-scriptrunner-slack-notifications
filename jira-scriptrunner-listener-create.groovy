42ddb9ba53f237b552326e67fc27df90f2997482
Kirill Ilinich <kilinich@gmail.com>
2018-03-25 15:13:52 +0300

    Optimizing

--- jira-scriptrunner-listener-create.groovy
+++ jira-scriptrunner-listener-create.groovy
@@ -1,29 +1,29 @@
 // Slack notification script for Jira Scriptrunner plugin by Kilinich
 // Usage: Paste in Script Listeners firing on Issue Created event
-//
-// Notification config, keys is case-sensitive!
+// New issue notification config, keys is case-sensitive!
+// Format: ['key':'channel', 'key':['channel1','channel2', ...], ...] 
 def notifyByIssueType = ['Incident':'#DevOps', 'Change request':'@kilinich', 'Service request':['@kilinich','@aolkhovsky']]
 def notifyByRequestType = ['Production incident':'#data-issues', 'Deploy request':'#deploy']
 def notifyByComponent = ['Jira Service Desk':'@kilinich']
-def notifyComponentOwner = ['Jira Service Desk']
-def icons = ['Incident':':incident:', 'Service request':':service:','Change request':':change:', 'Task':':task:','Sub-task':':task:']
 // Field IDs for our instance of Jira 
 def reqTypeField = 'customfield_10016'
+// Slack icons mapping by issue type
+def icons = ['Incident':':incident:', 'Service request':':service:','Change request':':change:', 'Task':':task:','Sub-task':':task:']
+def defaultIcon = ':incident:'
 // Method to post message via slack incoming webhook, url is stored in SLACK_WEBHOOK_URL in Scriptrunner global variables
 def postSlackMsg(toChannel, msg, attach ='') {
-    logger.info("Slack message to [$toChannel]: '${msg.take(80)}...' "+
-        post(SLACK_WEBHOOK_URL)
-            .header('Content-Type', 'application/json')
-            .body([
-                channel: toChannel,
-                mrkdwn: 'true',
-                text: msg,
-                attachments: [[text: attach]]
-            ])
-           .asString().statusText
-    )       
+    def resp = post(SLACK_WEBHOOK_URL)
+    .header('Content-Type', 'application/json')
+    .body([
+        channel: toChannel,
+        mrkdwn: 'true',
+        text: msg,
+        attachments: [[text: attach]]
+    ])
+    .asString().statusText
+    logger.info("Slack message to [$toChannel] '${msg.take(50)}...' $resp")       
 }
-// Get request issue type and type if exists, components and owners
+// Get request issue type and request type if exists, components 
 def issueType = issue.fields.issuetype.name
 def reqType = issue.fields."$reqTypeField" ? issue.fields."$reqTypeField".requestType.name : issueType
 def components = issue.fields.components.collect{it.name}
@@ -36,5 +36,5 @@ components.each{recipients.add(notifyByComponent[it])}
 recipients = recipients.flatten().unique() - null
 logger.info("Notifying recipients: $recipients") 
 recipients.each {
-    postSlackMsg(it,"${icons[issueType] ?: ':incident:'} *New ${issueType.toLowerCase()}*\n<$JIRA_SD_URL$issue.key|$issue.key: ${issue.fields.summary.take(80)}>\n$reqType from <@$issue.fields.reporter.name> with `$issue.fields.priority.name` priority require initial analisys.", issue.fields.description)
+    postSlackMsg(it,"${icons[issueType] ?: defaultIcon} *New ${issueType.toLowerCase()}*\n<$JIRA_SD_URL$issue.key|$issue.key: ${issue.fields.summary.take(80)}>\n$reqType from <@$issue.fields.reporter.name> with `$issue.fields.priority.name` priority require initial analisys.", issue.fields.description)
 }
--- jira-scriptrunner-listener-update.groovy
+++ jira-scriptrunner-listener-update.groovy
@@ -1,86 +1,60 @@
 // Slack notification script for Jira Scriptrunner plugin by Kilinich
 // Usage: Paste in Script Listeners firing on Issue Updated event
+// Notifying approvers and watchers
 //
-def issueTransit = false
-def issueReqApprov = false
-def issueDone = false
-def approversList = []
-def watchersList = []
 def doNotNotifyList = ['robot-manager']
+// Field IDs for our instance of Jira 
+def reqTypeField = 'customfield_10016'
+def approvalsField = 'customfield_10020'
 //
 // Method to post message via slack incoming webhook, url is stored in SLACK_WEBHOOK_URL in Scriptrunner global variables
-//
-def getReqType() {
-    if (issue.fields.customfield_10016.hasProperty('requestType')) {
-        issue.fields.customfield_10016.requestType.name
-    } else {
-        issue.fields.issuetype.name
-    }
-}
 def postSlackMsg(toChannel, msg, attach ='') {
-   logger.info("Posting message to $toChannel: '${msg.take(30)}...'")
-   post(SLACK_WEBHOOK_URL)
-            .header('Content-Type', 'application/json')
-            .body([
-                channel: toChannel,
-                mrkdwn: 'true',
-                text: msg,
-                attachments: [[text: attach]]
-            ])
-           .asString()
-}
-//
-// Detect if issue is just transitioned
-//
-def statusChange = changelog.items.find {it.field.equals('status')}
-if (statusChange) {
-     logger.info("Issue $issue.key transitioned from status [$statusChange.fromString] to [$statusChange.toString]")
-     issueTransit = true
-     if (statusChange.toString.equals('Done')) {issueDone = true}
-}
-// Detect if issue require approval and get approvers
-// customfield_10020 ("Approvals" in our currnt instance) contain info about all issue's aprovals
-if (issueTransit && issue.fields.customfield_10020) {
-
-    def pendingApproval = issue.fields.customfield_10020.find {it.finalDecision.equals('pending')}
-    if (pendingApproval) {
-        logger.info("Pending approval: $pendingApproval.id")
-    }
-
-    get("rest/api/2/issue/${issue.key}")
+    def resp = post(SLACK_WEBHOOK_URL)
     .header('Content-Type', 'application/json')
-    .asObject(Map).body.fields.customfield_10020
-    .each {
-        approvalItem ->
-        if (approvalItem.finalDecision.equals('pending')) {
-            issueReqApprov = true
-            logger.info("Approval ID $approvalItem.id decision: $approvalItem.finalDecision")
-            approvalItem.approvers.each {
-                approverItem -> approversList.add(approverItem.approver.name)
-            }
+    .body([
+        channel: toChannel,
+        mrkdwn: 'true',
+        text: msg,
+        attachments: [[text: attach]]
+    ])
+    .asString().statusText
+    logger.info("Slack message to [$toChannel] '${msg.take(50)}...' $resp")       
+}
+// Detect if issue is just transitioned
+if (changelog.items.find{it.field.equals('status')}) {
+    def issueType = issue.fields.issuetype.name
+    def reqType = issue.fields."$reqTypeField" ? issue.fields."$reqTypeField".requestType.name : issueType
+    def issueDone = changelog.items.find{it.field.equals('status')}.toString.equals('Done')
+    logger.info("Issue [$issueType] request [$reqType] is done: $issueDone")
+    // Detect if issue require approval and get approvers
+    def approversList = []
+    if (issue.fields."$approvalsField") {
+        // this is not used due to bug in Jira / Scriprunner - sometimes it's just null
+        def pendingApproval = issue.fields."$approvalsField".find{it.finalDecision.equals('pending')}
+        if (pendingApproval) {logger.info("Pending approval (static debug): $pendingApproval.id")}
+        // get from REST API instead of static objects
+        pendingApproval = get("rest/api/2/issue/$issue.key")
+        .header('Content-Type', 'application/json')
+        .asObject(Map).body.fields."$approvalsField".find{it.finalDecision.equals('pending')}
+        if (pendingApproval) {
+            logger.info("Pending approval: $pendingApproval.id")
+            approversList = pendingApproval.approvers.collect{it.approver.name}
             logger.info("Issue require approval of: $approversList")
         }
     }
-}
-//
-// Notify approvers
-//
-if (issueReqApprov) {
+    // Notify approvers if exists
     approversList.each { username ->
-    postSlackMsg("@$username", ":nerd: *Your approval required*\n<$JIRA_SD_URL$issue.key|$issue.key: ${issue.fields.summary.take(80)}>\n${getReqType()} from <@$issue.fields.reporter.name> transitioned to `$issue.fields.status.name` status by <@$user.name> and now require your action to ${if (issueDone) 'close' else 'move on'}.", issue.fields.description)
+    postSlackMsg("@$username", ":nerd: *Your approval required*\n<$JIRA_SD_URL$issue.key|$issue.key: ${issue.fields.summary.take(80)}>\n$reqType from <@$issue.fields.reporter.name> transitioned to `$issue.fields.status.name` status by <@$user.name> and now require your action to ${if (issueDone) 'close' else 'move on'}.", issue.fields.description)
     }
-}
-//
-// Notify watchers (except approvers and initiator) and initiator if issue is done
-//
-if (issueDone) {
-// Get issue watchers
-    get("rest/api/2/issue/${issue.key}/watchers")
+    // Notify watchers (except approvers and event initiator) if issue is done
+    if (issueDone) {
+        def watchersList = get("rest/api/2/issue/$issue.key/watchers")
         .header('Content-Type', 'application/json')
-        .asObject(Map).body.watchers.each {watcher -> watchersList.add(watcher.name)}
-    logger.info("Watchers: $watchersList")
-    (watchersList - doNotNotifyList - user.name - issue.fields.reporter.name).each {
-        watcher ->
-        postSlackMsg("@$watcher", ":coffee: *Work is done*\n<$JIRA_SD_URL$issue.key|$issue.key: ${issue.fields.summary.take(80)}>\n${getReqType()} from <@$issue.fields.reporter.name> resolved by <@$user.name>, you are watching it so take a look if you want.",issue.fields.description)
+        .asObject(Map).body.watchers.collect{it.name}
+        logger.info("Watchers: $watchersList")
+        (watchersList - doNotNotifyList - user.name - approversList).each {
+            watcher ->
+            postSlackMsg("@$watcher", ":coffee: *Work is done*\n<$JIRA_SD_URL$issue.key|$issue.key: ${issue.fields.summary.take(80)}>\n$reqType from <@$issue.fields.reporter.name> resolved by <@$user.name>, you are watching it so take a look if you want.", issue.fields.description)
+        }
     }
 }
